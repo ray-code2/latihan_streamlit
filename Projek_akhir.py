777ef202af1c8979fc5f05f7b49f8e0dfff9031a
ray-code2 <raymondtjahyadi00@gmail.com>
2022-04-07 11:29:30 +0700

    Commit Pertama

---
+++ Projek_akhir.py
@@ -0,0 +1,8 @@
+# Membuat EDA menggunakan streamlit untuk memvisualisasikan data
+# Mengambil insights dalam data 
+# Membuat Model Prediksi data 
+# Nama Anggota Tim :
+# 1. Raymond Tjahyadi
+
+#import Libraries yang dibutuhkan
+import streamlit as st
